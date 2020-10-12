6b1a2ea3b28066ef85f9b2f95011e8c9a3619611
Melvyn Guéroult <github@melvyn.eu>
2020-10-12 22:15:08 +0200

    First Init

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log("Hello World !");
+});
+
+bot.login("NzY1Mjg1NDA3NDI4MTgyMDM2.X4Slkw.vlOy7wX7_qmPXcmCaw_rBDrO1uU");
